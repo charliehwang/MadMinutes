4639d04751ba8427c46a10d3c95082ce767c2b25
charlieh <charliehwang@gmail.com>
2019-10-29 19:36:48 -0600

    Added pathPrefix

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,7 @@ module.exports = {
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
   },
-  // pathPrefix: "/madminutes.github.io",
+  pathPrefix: "/madminutesSite",
   plugins: [
     `gatsby-plugin-react-helmet`,
     {
